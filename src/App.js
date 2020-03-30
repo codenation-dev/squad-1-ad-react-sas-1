c53424f33b49f90a6282be12dd6d5f6399a451e8
Kelvin Silva <kelvinsilva.ksv@gmail.com>
2020-03-30 13:26:37 -0300

    criacao component container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Container from './components/Container'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid style={{ backgroundColor: 'tomato', height: 120, color: 'white' }}>
+        <div>Aqui um exemplo de container centralizado com 100% da tela</div>
+        <br />
+        <code>
+          {`<Container fluid>
+              elementos html aqui
+            </Container>
+          `}
+        </code>
+      </Container>
+      <Container style={{ backgroundColor: 'lightblue', height: 120, color: '#444' }}>
+        Aqui um exemplo de container centralizado
+        <br />
+        <code>
+          {`<Container >
+              elementos centralizados do html aqui
+            </Container>
+          `}
+        </code>
+      </Container>
+      <Container  className="bg-green" fluid style={{ height: 120, color: '#444' }}>
+        <div>Aqui um exemplo adicionando estilos via className</div>
+        <br />
+        <code>
+          {`<Container className="minha-classe">
+            elementos centralizados do html aqui
+            </Container>
+          `}
+        </code>
+      </Container>
     </div>
   );
 }
