cf353d07b61d8d21909e6122c69cb2e33aa8d7ec
Francisco Junior <jr-francis2011@hotmail.com>
2020-04-06 22:58:07 -0300

    Foi feito a criaçao do catalogo de produtos

--- src/App.js
+++ src/App.js
@@ -2,40 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Container from './components/Container'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Container fluid style={{ backgroundColor: 'tomato', height: 120, color: 'white' }}>
-        <div>Aqui um exemplo de container centralizado com 100% da tela</div>
-        <br />
-        <code>
-          {`<Container fluid>
-              elementos html aqui
-            </Container>
-          `}
-        </code>
-      </Container>
-      <Container style={{ backgroundColor: 'lightblue', height: 120, color: '#444' }}>
-        Aqui um exemplo de container centralizado
-        <br />
-        <code>
-          {`<Container >
-              elementos centralizados do html aqui
-            </Container>
-          `}
-        </code>
-      </Container>
-      <Container  className="bg-green" fluid style={{ height: 120, color: '#444' }}>
-        <div>Aqui um exemplo adicionando estilos via className</div>
-        <br />
-        <code>
-          {`<Container className="minha-classe">
-            elementos centralizados do html aqui
-            </Container>
-          `}
-        </code>
-      </Container>
+  <div>
+    <Home>
+
+    </Home>
+
+    {/* // <div>
+    //   <Container fluid style={{ backgroundColor: 'tomato', height: 120, color: 'white' }}>
+    //     <div>Aqui um exemplo de container centralizado com 100% da tela</div>
+    //     <br />
+    //     <code>
+    //       {`<Container fluid>
+    //           elementos html aqui
+    //         </Container>
+    //       `}
+    //     </code>
+    //   </Container>
+    //   <Container style={{ backgroundColor: 'lightblue', height: 120, color: '#444' }}>
+    //     Aqui um exemplo de container centralizado
+    //     <br />
+    //     <code>
+    //       {`<Container >
+    //           elementos centralizados do html aqui
+    //         </Container>
+    //       `}
+    //     </code>
+    //   </Container>
+    //   <Container  className="bg-green" fluid style={{ height: 120, color: '#444' }}>
+    //     <div>Aqui um exemplo adicionando estilos via className</div>
+    //     <br />
+    //     <code>
+    //       {`<Container className="minha-classe">
+    //         elementos centralizados do html aqui
+    //         </Container>
+    //       `}
+    //     </code>
+    //   </Container>
+    // </div> */}
     </div>
   );
 }
