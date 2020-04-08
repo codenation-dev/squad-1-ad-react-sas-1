ab05a1f98a59eb27234b07d2fb89b9d51525fe52
Francisco Junior <jr-francis2011@hotmail.com>
2020-04-07 22:20:32 -0300

    Criaçao do componente de catalogo

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -1,69 +1,84 @@
-import React from 'react';
-import Container from '../Container';
-import ProductCatalog from '../ProductCatalog';
-import './style.scss';
+import React from "react";
+import Container from "../Container";
+import ProductCatalog from "../ProductCatalog";
+import "./style.scss";
 
 class Catalog extends React.Component {
-    constructor(props) {
-        super(props)
-    };
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-        <div className="catalog">
-            <Container className="catalog__itens">
-                <ProductCatalog product={{ 
-                    porcentage: 10, 
-                    name: "bolsa", 
-                    price: 190.90, 
-                    priceDiscount: 159.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoBolsa"}} 
-                />
+  render() {
+    return (
+      <div className="catalog">
+        <Container className="catalog__itens">
+          <ProductCatalog
+            product={{
+              percentage: 10,
+              name: "bolsa",
+              price: 190.9,
+              priceDiscount: 159.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoBolsa",
+            }}
+          />
 
-                <ProductCatalog product={{ 
-                    porcentage: 15, 
-                    name: "calça", 
-                    price: 190.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca" }} 
-                />
+          <ProductCatalog
+            product={{
+              percentage: 15,
+              name: "calça",
+              price: 190.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca",
+            }}
+          />
 
-                <ProductCatalog product={{ 
-                    porcentage: 10, 
-                    name: "bolsa", 
-                    price: 190.90, 
-                    priceDiscount: 159.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoBolsa"}} 
-                />
+          <ProductCatalog
+            product={{
+              percentage: 10,
+              name: "bolsa",
+              price: 190.9,
+              priceDiscount: 159.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoBolsa",
+            }}
+          />
 
-                <ProductCatalog product={{ 
-                    porcentage: 15, 
-                    name: "calça", 
-                    price: 190.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca" }} 
-                />
+          <ProductCatalog
+            product={{
+              percentage: 15,
+              name: "calça",
+              price: 190.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca",
+            }}
+          />
 
-                <ProductCatalog product={{ 
-                    porcentage: 15, 
-                    name: "calça", 
-                    price: 190.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca" }} 
-                />
-                <ProductCatalog product={{ 
-                    porcentage: 15, 
-                    name: "calça", 
-                    price: 190.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca" }} 
-                />
+          <ProductCatalog
+            product={{
+              percentage: 15,
+              name: "calça",
+              price: 190.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca",
+            }}
+          />
+          <ProductCatalog
+            product={{
+              percentage: 15,
+              name: "calça",
+              price: 190.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca",
+            }}
+          />
 
-                <ProductCatalog product={{ 
-                    porcentage: 15, 
-                    name: "calça", 
-                    price: 190.90, 
-                    img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca" }} 
-                />
-            </Container>
-        </div>);
-    }
-};
+          <ProductCatalog
+            product={{
+              percentage: 15,
+              name: "calça",
+              price: 190.9,
+              img: "https://via.placeholder.com/200x300.png?text=ProdutoCalca",
+            }}
+          />
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Catalog;
--- src/components/ProductCatalog/index.jsx
+++ src/components/ProductCatalog/index.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import './style.scss';
+import React from "react";
+import Badge from "../Badge";
+import "./style.scss";
 
 class ProductCatalog extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-   
-    render() {
-        const { name, img, porcentage, price, priceDiscount } = this.props.product;
-        const hasDiscount = priceDiscount ? "product-catalog has-discount":"product-catalog"
+  constructor(props) {
+    super(props);
+  }
 
-        return(
-        <div className={hasDiscount}>
-            <div className="product-catalog__image-wrapper">
-                <img src={img}/>
-                <div className="product-catalog__badge">
-                    {porcentage}%
-                </div>
-            </div>
-            <div className="product-catalog__description">
-                <div className="product-catalog__product-name">{name}</div>
-                <div className="product-catalog__product-prices">
-                    <span className="product-catalog__product-price is-regular">{price}</span>
-                    <span className="product-catalog__product-price is-discount">{priceDiscount}</span>
-                </div>
-            </div>
+  render() {
+    const { name, img, price, priceDiscount, percentage } = this.props.product;
+    const hasDiscount = priceDiscount
+      ? "product-catalog has-discount"
+      : "product-catalog";
+
+    return (
+      <div className={hasDiscount}>
+        <div className="product-catalog__image-wrapper">
+          <Badge percentage={percentage} />
+          <img src={img} />
         </div>
-        )
-    };
-};
+        <div className="product-catalog__description">
+          <div className="product-catalog__product-name">{name}</div>
+          <div className="product-catalog__product-prices">
+            <span className="product-catalog__product-price is-regular">
+              {price}
+            </span>
+            <span className="product-catalog__product-price is-discount">
+              {priceDiscount}
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ProductCatalog;
