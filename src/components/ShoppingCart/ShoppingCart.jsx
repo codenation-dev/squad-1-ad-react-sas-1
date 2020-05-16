2c53a5bf7a68999bace9582467a564e4a9e1c9bc
kfurfles <kelvinsilva.ksv@gmail.com>
2020-05-16 11:31:05 -0300

    ajustes na estruturas de pastas

---
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Cart from '../Cart'
+import CartItem from '../CartItem';
+import './style.scss'
+import CartFooter from "../CartFooter";
+
+const ShoppingCart = ({closeShoppingCart, active}) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <Cart
+      onClose={closeShoppingCart}
+      title={`sacola(${arr.length})`}
+      active={active}
+    >
+      <div className="shopping-cart__content">
+        <ul className="shopping-cart__content-full">
+          {
+            arr.map((item,idx) => (
+              <CartItem
+                key={idx}
+                title="Teste"
+                price="12"
+                size="M"
+              />
+            ))
+          }
+        </ul>
+        <div className="shopping-cart__content-empty">
+           <span>
+               Seu carrinho está vazio :(
+           </span>
+        </div>
+      </div>
+
+      <CartFooter/>
+    </Cart>
+  )
+}
+
+export default ShoppingCart
