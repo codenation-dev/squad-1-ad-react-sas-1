74ce2f4be936119488fd965ce3eac3ebe7b9597d
Kfurfles <kelvinsilva.ksv@gmail.com>
2020-05-23 04:32:11 -0300

    fallback pagina de produtos

--- src/containers/ProductContainer/ProductContainer.jsx
+++ src/containers/ProductContainer/ProductContainer.jsx
@@ -1,41 +1,58 @@
 import React, {useEffect, useState} from "react";
-import {useLocation} from 'react-router-dom'
+import {useLocation, useHistory} from 'react-router-dom'
 import {useSelector} from "react-redux";
 import {findProductByNameAndSlug} from "../../modules/redux/cart/helpers";
 import Loader from "../../components/Loader";
+import debouce from 'lodash.debounce'
 
 import './style.scss'
 import ProductFullContent from "../../components/ProductFullContent";
+import {delay} from "../../modules/time";
 
 const ProductContainer = () => {
   const location = useLocation()
+  const history = useHistory()
+  const [initialized, setInitialized] = useState(false)
+  const [output, setOuput] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState(null)
   const { pathname, search } = location
 
   const productName = pathname.substring(9)
   const slug = search.substring(7)
-  let selectedProduct = null
 
   const {
     product, items
   } = useSelector(({ Products : { product, items } }) => ({  product, items }))
 
-  let output;
+  useEffect(() => {
+    items.length ? setInitialized(true) : setInitialized(false)
+  },[items])
 
-  if(product){
-    selectedProduct = product
-  } else {
-    selectedProduct = findProductByNameAndSlug(productName,slug,items)
-  }
+  useEffect(() => {
+    if(initialized){
+      if(product){
+        setSelectedProduct(product)
+      } else {
+        setSelectedProduct(findProductByNameAndSlug(productName,slug,items))
+      }
 
-  if(selectedProduct){
-    output = <ProductFullContent product={selectedProduct} />
-  } else {
-    output = <div className="product-page__loader">
-      <Loader />
-    </div>
-  }
+      if(selectedProduct){
+        setOuput(<ProductFullContent product={selectedProduct} />)
+      } else {
+        setOuput(<div className="product-page__loader">
+          <Loader />
+        </div>)
+      }
 
-  return (
+      setTimeout(() => {
+        if(!selectedProduct && !product){
+          history.push('/404')
+        }
+      }, 2000)
+    }
+  },[initialized, selectedProduct, items])
+
+    return (
     <div className="product-page">
       {output}
     </div>
